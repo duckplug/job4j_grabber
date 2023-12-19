9d8d91445da524cc4b161e1f2ba19f3d3f51f364
Anton <okolelovant@gmail.com>
2023-12-19 15:56:18 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/quartz/HabrCareerParse.java
+++ src/main/java/ru/job4j/quartz/HabrCareerParse.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 
 public class HabrCareerParse {
     /* У нас есть три константы. Первая это ссылка на сайт в целом.
@@ -20,33 +21,34 @@ public class HabrCareerParse {
     public static void main(String[] args) throws IOException {
         /* Сначала мы формируем ссылку с учетом номера страницы и получаем саму страницу,
          чтобы с ней можно было работать */
-        int pageNumber = 1;
-        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-        Connection connection = Jsoup.connect(fullLink);
-        Document document = connection.get();
+        for (int pageNumber = 1; pageNumber < 6; pageNumber++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            Connection connection = Jsoup.connect(fullLink);
+            Document document = connection.get();
         /* Далее анализируя структуру страницы мы выясняем,
          что признаком вакансии является CSS класс .vacancy-card__inner,
          а признаком названия класс .vacancy-card__title
          На основе анализа прописываем парсинг.
          Сначала мы получаем все вакансии страницы
          */
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
             /*
             Получение времени публикации вакансии
              */
-            Element date = row.select(".vacancy-card__date").first();
-            Element dateTime = date.child(0);
-            String vacancyDate = dateTime.attr("datetime");
+                Element date = row.select(".vacancy-card__date").first();
+                Element dateTime = date.child(0);
+                String vacancyDate = dateTime.attr("datetime");
             /* Получаем данные. text() возвращает все содержимое элемента в виде текста,
              т.е. весь текст что находится вне тегов HTML. Ссылка находится в виде атрибута,
               поэтому ее значение надо получить как значение атрибута.
               Для этого служит метод attr() */
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", vacancyName, vacancyDate, link);
-        });
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s%n", vacancyName, vacancyDate, link);
+            });
+        }
     }
 }
